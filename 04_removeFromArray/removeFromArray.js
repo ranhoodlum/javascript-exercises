4dfd060d58ec161670e5c98f77f9b106bc49cddf
srj-17 <saugatrijal17@gmail.com>
2024-06-19 08:41:33 +0545

    Improve 04.removeFromArray solution
    Apply pre-exisiting methods to solve the problem instead
    of what I did before.
    Creating newArray and pushing the items not in the removal
    list solves the problem created of adjusting the array size
    after removal, as done in my previous solution.
    The provided solution with array.filter() method provides
    even better solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,22 +1,12 @@
-const removeFromArray = function(numbers, ...removeNumbers) {
-    let removedCount = 0;
-
-    // we need this because later the size of array decreases when numbers are removed, 
-    // but we need to iterate through all the given numbers and not break out of loop early
-    const len = numbers.length
-    for (let i = 0; i < len; i++) {
-
-        // array moves one step left (shifts) each tiem a number is removed from it
-        for (const no of removeNumbers) {
-            if (numbers[i-removedCount] === no) {
-                numbers.splice(i-removedCount, 1);
-                removedCount++;
-                break;
-            }
+const removeFromArray = function (numbers, ...args) {
+    newArray = []
+    numbers.forEach(item => {
+        if (!args.includes(item)) {
+            newArray.push(item);
         }
-    }
-    return numbers;
-};
+    });
+    return newArray;
+} 
 
 // Do not edit below this line
 module.exports = removeFromArray;
