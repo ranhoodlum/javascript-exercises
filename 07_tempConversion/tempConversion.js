467f0d437f882e0edf727292250ada5bb3f0f335
srj-17 <saugatrijal17@gmail.com>
2024-06-19 21:11:05 +0545

    Do exercises for loops and arrays

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+
+const convertToCelsius = function(fahrenheitTemp) {
+  return round(((fahrenheitTemp - 32) * 5 / 9), 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  return round(((celsiusTemp * 9 / 5 + 32)), 1);
 };
 
 // Do not edit below this line
