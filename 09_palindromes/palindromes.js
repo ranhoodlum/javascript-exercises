de923a2b896c6bdc1aec293774ff1c4fb191ef10
ranhoodlum <ranhoodlum17@gmail.com>
2024-06-24 17:47:08 +0545

    Do 09 exercise
    Can be done with the array methods without using regex
    Check Solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    // remove all punctuation and word-breaks
+    pureText = text.replaceAll(/[ !\.,-]/g, '').toLowerCase();
+    console.log(pureText);
+    console.log(pureText.split('').reverse().join(''));
+    return (pureText.split('').reverse().join('') === pureText);
 };
 
 // Do not edit below this line
