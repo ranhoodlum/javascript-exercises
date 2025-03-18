7dd016645f2ba1261563dc958331789f7db2fed8
ranhoodlum <ranhoodlum17@gmail.com>
2025-03-18 09:36:00 +0545

    feat: Complete exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(((sum, element) => sum + element), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(((mul, element) => mul * element), 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return (b === 0) ? 1 : a * power(a, b-1);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	return (a === 0) ? 1 : a * factorial(a - 1);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    return people.reduce( (oldest, person) => {
+        let oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        let personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        console.log(oldestAge);
+        console.log(personAge);
+        return (personAge > oldestAge) ? person : oldest;
+    });
 };
 
 // Do not edit below this line
